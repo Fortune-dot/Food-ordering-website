cb86824929b7cc9e086fb92e37b89fd13ce6560f
devphilip023 <108599963+devphilip023@users.noreply.github.com>
2022-07-08 05:02:47 -0700

    few changes

--- partials-front/footer.php
+++ partials-front/footer.php
@@ -12,7 +12,7 @@
                     <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/>twittet</a>
                 </li>
                 <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/"/><a href="tel:+1123-456-7890">072475127</a></a>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/"/><a href="tel:+1123-456-7890">0701253137</a></a>
                 </li>
             </ul>
         </div>
